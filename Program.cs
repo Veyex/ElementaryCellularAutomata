02a5815aae8798f6fa7dadf0973e6f38fcb0618f
Veyex <79301840+Veyex@users.noreply.github.com>
2023-02-03 16:20:31 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+namespace ElementaryCellularAutomata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            eCA eca = new eCA();
+            eCA.Init();
+        }
+    }
+}
---
+++ eCA.cs
@@ -0,0 +1,32 @@
+﻿namespace ElementaryCellularAutomata
+{
+    class eCA
+    {
+        #region Properties & Fields
+        public int length;
+        public int rule;
+        public int[] cellArray;
+        public int[] neighbourArray;
+        #endregion
+
+        #region Methods
+        public void Init()
+        {
+            Console.WriteLine("Enter Length:");
+            length = Convert.ToInt32(Console.ReadLine());
+            cellArray = new int[length];
+            Console.WriteLine("Enter Rule:");
+        }
+        public void CalculateNextGeneration()
+        {
+            for (int target = 0; target < length; target++)
+            {
+                for (int neighbour = target - 1; neighbour <= target + 1; neighbour++)
+                {
+
+                }
+            }
+        }
+        #endregion
+    }
+}
